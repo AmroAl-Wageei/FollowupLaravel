2d7e7cc4aa87687ab1a2226d366814255a664503
amro alwageei  -gt <gt.amro.alwageei@Jt.JtGroup>
2023-09-05 15:46:02 +0300

    Comment

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Create Larvel Project
+// ** composer create-project laravel/laravel example-app
+// ** composer global require laravel/installer , ** laravel new example-app
+
+// php artisan serve --port=8004
+ 
+
+
+
+// php artisan serve
+// php artisan make:migration create_students_table --> "Migration"
+// php artisan make:controller Student              --> "Controller"
+// php artisan make:model Student                   --> "Model"
+
+// php artisan make:model Student -m         --> Create Model and migration
+// php artisan make:model Student -mc        --> Create Model , migration and controller
+// php artisan make:model Student -mcr       --> Create Model , migration and controller " Resource "
+
+
+// php artisan make:model Student -mcr
+// php artisan make:model Teacher -mcr
+
+
+// Two way to CRUD App ( Query - Elequent ) .....
+
+// Render Route - View 
+// Render Route - Controller -  View 
+// Render Route - Controller - Model -   View 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get( '/test',[StudentController::class , "index"]);
+Route::resource('/testt',[StudentController::class]);
+
+
+
+Route::get('/student', function () {
+    return view('student');
+});
+
+Route::get('/teacher', function () {
+    return view('teacher');
+});
